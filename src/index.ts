84aff95ee8962ff1d72c0d23fbc4c94172b51368
gilles30 <lorison.gilles@gmail.com>
2022-11-28 18:29:50 -0300

    ☠️feat: added websockets in api

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
 import path from 'node:path';
+import http from 'node:http';
 import express from 'express';
 import mongoose from 'mongoose';
+import { Server } from 'socket.io';
 
 import { router } from './router';
+
+const app = express();
+const server = http.createServer(app);
+export const io = new Server(server);
 
 mongoose
   .connect('mongodb://localhost:27017')
   .then(() => {
-    const app = express();
     const port = 8080;
+
     app.use((req, res, next) => {
       // dommen http for permissions exemple localhost:8080
       res.setHeader('Access-Control-Allow-Origin', '*');
@@ -27,7 +33,7 @@ mongoose
     app.use(express.json());
     app.use(router);
 
-    app.listen(port, () => {
+    server.listen(port, () => {
       console.log(`🎩 Server is running on http://localhost:${port}`);
     });
   })
