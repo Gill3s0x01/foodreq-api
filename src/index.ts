0ba91a9bf8c21cd8bf3f3e87644ffc2921b49005
gilles30 <lorison.gilles@gmail.com>
2022-11-28 15:07:54 -0300

    feat: added bypass cors

--- src/index.ts
+++ src/index.ts
@@ -9,14 +9,22 @@ mongoose
   .then(() => {
     const app = express();
     const port = 8080;
+    app.use((req, res, next) => {
+      // dommen http for permissions exemple localhost:8080
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      //methods for permissions
+      res.setHeader('Access-Control-Allow-Methods', '*');
+      res.setHeader('Access-Control-Allow-Headers', '*');
 
-    app.use(express.json());
+      next();
+    });
 
     app.use(
       '/uploads',
       express.static(path.resolve(__dirname, '..', 'uploads'))
     );
 
+    app.use(express.json());
     app.use(router);
 
     app.listen(port, () => {
